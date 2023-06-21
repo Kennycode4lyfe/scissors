fb30f5b49e475a0cb7bf065001f596bff70f9c4b
Adediran Kehinde <kennydemola2@gmail.com>
2023-06-21 13:29:39 +0100

    txt file added to qrcodes folder

--- saveToRedis.js
+++ saveToRedis.js
@@ -1,44 +1,39 @@
 const { createClient } = require("redis");
-require('dotenv').config()
+require("dotenv").config();
 
-const REDIS_HOST=process.env.REDIS_HOST
-const REDIS_PORT= process.env.REDIS_PORT
-const REDIS_PASSWORD= process.env.REDIS_PASSWORD||'default'
-const REDIS_USERNAME= process.env.REDIS_USERNAME
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD || "default";
+const REDIS_USERNAME = process.env.REDIS_USERNAME;
 
-
-const client = createClient({url:`redis://${REDIS_USERNAME}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`});
+const client = createClient({
+  url: `redis://${REDIS_USERNAME}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`,
+});
 
 class Redis {
-  async connectToRedis(){
-
+  async connectToRedis() {
     client.on("error", (err) => console.log("Redis Client Error", err));
-    client.on('connect',()=>console.log('connected to redis server'))
-   await client.connect()
-}
-
-  async setCache(key,value){
-    await client.set(key,value)
-    console.log('cache hit')
-
+    client.on("connect", () => console.log("connected to redis server"));
+    await client.connect();
   }
 
-  
-  async getCache(key){
-   const urlDetails = await client.get(key)
-    console.log('cache hit')
-    return urlDetails
-
-  }
-  async deleteCache(key){
-    await client.del(key)
+  async setCache(key, value) {
+    await client.set(key, value);
+    console.log("cache hit");
   }
 
-async deleteAllCache(){
-  await client.flushAll()
+  async getCache(key) {
+    const urlDetails = await client.get(key);
+    console.log("cache hit");
+    return urlDetails;
+  }
+  async deleteCache(key) {
+    await client.del(key);
+  }
+
+  async deleteAllCache() {
+    await client.flushAll();
+  }
 }
 
-}
-
-
-module.exports = {Redis}
+module.exports = { Redis };
